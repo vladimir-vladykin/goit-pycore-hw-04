a42fa7dce05a1b8f246a51352a4025123fb9479d
Volodymyr Vladykin <vova@pocketguard.com>
2024-07-15 20:10:27 +0300

    Added colorama lib; added rendering of dir items with color; better depth indicator (dots were not the best choise)

--- dir_rendering.py
+++ dir_rendering.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+from colorama import Fore, Back, Style
 
+colors_pallete = (Fore.CYAN, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.YELLOW)
+depth_step = 4
+depth_indicator = "~"
 
 def visualize_dir(path: str):
     directory = Path(path)
@@ -8,11 +12,18 @@ def visualize_dir(path: str):
         print(f"{path} is not a dir, we cannot visualize it")
         return
     
+    # adjust background and style once here.
+    # text color will be adjusted dynamically for each item
+    print(Back.WHITE)   
+    print(Style.BRIGHT)
+
     render_dir(directory)
     
 
 def render_dir(directory: Path, depth_level: int = 0):
     render_item(directory.name, depth_level)
+
+    # iterate through all directly and render all we found
     for dir_item in directory.iterdir():
         if dir_item.is_file():
             render_item(dir_item.name, depth_level + 1)
@@ -20,11 +31,19 @@ def render_dir(directory: Path, depth_level: int = 0):
             render_dir(dir_item, depth_level + 1)
 
 def render_item(item_name: str, depth_level: int):
-    # TODO are dots OK considering there's hidden files?
-    prefix = "" if depth_level == 0 else "." * (depth_level * 4)
+    # select text color based on current depth
+    adjust_text_color(depth_level)
+    
+    # format prefix to better visualize depth of item
+    prefix = "" if depth_level == 0 else depth_indicator * (depth_level * depth_step)
+
     print(f"{prefix}{item_name}")
+
+def adjust_text_color(depth_level):
+    print(colors_pallete[depth_level % len(colors_pallete)])
 
 
 if __name__ == "__main__":
     # TODO rename to main, and parse args inside?
-    visualize_dir("")
+    # visualize_dir("/Users/volodymyr_vl/reports_sent")
+    visualize_dir(".")
