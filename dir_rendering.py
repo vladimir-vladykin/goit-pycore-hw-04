8290d8e3be74fe1262a804163e814ae7526a0f80
Volodymyr Vladykin <vova@pocketguard.com>
2024-07-15 20:23:12 +0300

    Use terminal arguments as a source of path to directory

--- dir_rendering.py
+++ dir_rendering.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from colorama import Fore, Back, Style
 
@@ -5,7 +6,7 @@ colors_pallete = (Fore.CYAN, Fore.GREEN, Fore.BLUE, Fore.MAGENTA, Fore.YELLOW)
 depth_step = 4
 depth_indicator = "~"
 
-def visualize_dir(path: str):
+def main(path: str):
     directory = Path(path)
 
     if not directory.is_dir():
@@ -21,7 +22,7 @@ def visualize_dir(path: str):
     
 
 def render_dir(directory: Path, depth_level: int = 0):
-    render_item(directory.name, depth_level)
+    render_item(directory.name + "/", depth_level)
 
     # iterate through all directly and render all we found
     for dir_item in directory.iterdir():
@@ -29,6 +30,7 @@ def render_dir(directory: Path, depth_level: int = 0):
             render_item(dir_item.name, depth_level + 1)
         if dir_item.is_dir():
             render_dir(dir_item, depth_level + 1)
+
 
 def render_item(item_name: str, depth_level: int):
     # select text color based on current depth
@@ -39,11 +41,15 @@ def render_item(item_name: str, depth_level: int):
 
     print(f"{prefix}{item_name}")
 
+
 def adjust_text_color(depth_level):
     print(colors_pallete[depth_level % len(colors_pallete)])
 
 
 if __name__ == "__main__":
-    # TODO rename to main, and parse args inside?
-    # visualize_dir("/Users/volodymyr_vl/reports_sent")
-    visualize_dir(".")
+    # we expect first arg after script name is gonna be path to the dir
+    if len(sys.argv) > 1:
+        dir_path = sys.argv[1]
+        main(dir_path)
+    else:
+        print("No path found. Make sure to put path to directory as first parameter of this script")
